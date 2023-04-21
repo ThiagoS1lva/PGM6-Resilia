5df065256fa5629b20b7feea9498a3f2692e5e63
Thiago Oliveira <thiagooliveira745@gmail.com>
2023-04-21 16:27:45 -0300

    Menu

--- src/frontend/components/Menu.jsx
+++ src/frontend/components/Menu.jsx
@@ -2,6 +2,8 @@ import styles from './Menu.module.css'
 import './Navbar.css'
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png'
+import icone from '../assets/icone_perfil.png'
 
 function Menu() {
     const [isTransparent, setTransparent] = useState(false);
@@ -23,15 +25,23 @@ function Menu() {
     }
 
     return (
-        <nav className={isTransparent ? "transparent" : "color"}>
-            <ul className={`ul ${isTransparent ? "teste" : ""}`}>
-                <Link to="/"><li>Página Inicial</li></Link>
-                <li>Sobre</li>
-                <li>Contato</li>
-                <li>Pontos de coleta</li>
-                <Link to="/login"><li>Login/Cadastro</li></Link>
-            </ul>
-        </nav>
+
+        <>
+            <nav className={isTransparent ? "transparent" : "color"}>
+                <div>
+                    <Link to="/"><img className={styles.img_logo} src={logo} width='70px' /></Link>
+                </div>
+                <ul className={`ul ${isTransparent ? "teste" : ""}`}>
+                    <Link to="/"><li>Página Inicial</li></Link>
+                    <li>Sobre</li>
+                    <li>Contato</li>
+                    <li>Pontos de coleta</li>
+                    <li className={styles.img_perfil}><img width='100%' src={icone} /></li>
+                    <Link to="/login"><li>Login / Cadastro</li></Link>
+                </ul>
+            </nav>
+            <div className={styles.glass}></div>
+        </>
     )
 }
 
--- src/frontend/pages/Login.jsx
+++ src/frontend/pages/Login.jsx
@@ -3,7 +3,17 @@ import styles from './Login.module.css';
 function Login () {
 
     return (
-        <h1>Oi</h1>
+        <>
+            <div className={styles.container}>
+                <div className={styles.containerLogin}>
+                    <h1>Login</h1>
+                </div>
+
+                <div className={styles.containerImage}>
+                    <h1>Imagens</h1>
+                </div>
+            </div>
+        </>
     )
 }
 
