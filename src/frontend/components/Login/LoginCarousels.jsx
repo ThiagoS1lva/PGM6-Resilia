835576a0ef69c05e7bec3100e288e86c20a50149
Thiago Oliveira <thiagooliveira745@gmail.com>
2023-04-21 18:54:38 -0300

    carrossel

--- src/frontend/components/Login/LoginCarousels.jsx
+++ src/frontend/components/Login/LoginCarousels.jsx
@@ -1,10 +1,40 @@
 import { Carousel } from 'react-bootstrap';
+import img1 from '../../assets/carrossel/1.png';
+import img2 from '../../assets/carrossel/2.png'
+import img3 from '../../assets/carrossel/3.png'
 
-function LoginCarousels () {
+function LoginCarousels() {
     return (
-        <h1>
-            Oi
-        </h1>
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={img1}
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={img2}
+                />
+                <div style={{textAlign:'center'}}>
+                    <h3>Second slide label</h3>
+                </div>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={img3}
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     )
 }
 export default LoginCarousels
--- src/frontend/pages/Login.jsx
+++ src/frontend/pages/Login.jsx
@@ -1,32 +1,77 @@
 import styles from './Login.module.css';
 import { Link } from 'react-router-dom';
 import LoginCarousels from '../components/Login/LoginCarousels';
-
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
 function Login() {
+    const [cliente, setCliente] = useState(true);
+
+    function handleEmpresa() {
+        setCliente(false);
+    }
+    function handleCliente() {
+        setCliente(true);
+    }
 
     return (
         <>
             <div className={styles.container}>
-                <div className={styles.containerLogin}>
-                    <h1>Login</h1>
-                    <form className={styles.form}>
-                        <label>Digite seu email</label>
-                        <input type="email" placeholder="Digite seu email" />
-                        <div className={styles.label}>
-                            <label>Digite sua senha</label>
-                            <Link to="/"><label style={{cursor
-                            : "pointer"}}>Esqueceu sua senha?</label></Link>
+                {cliente ?
+                    <div className={styles.containerLogin}>
+                        <h1>Login</h1>
+                        <div className={styles.btnCE}>
+                            <Button onClick={handleCliente}>Cliente</Button>
+                            <Button variant="outline-secondary" onClick={handleEmpresa}>Empresa</Button>
                         </div>
-                        <input type="password" placeholder="Digite sua senha" />
-                        <p>Não tem uma conta?</p>
-                        <Link to="/cadastre-se">Cadastre-se</Link>
-                        <button role='button'>Logar</button>
-                    </form>
-                </div>
+                        <form className={styles.form}>
+                            <label>Digite seu email</label>
+                            <input type="email" placeholder="Digite seu email" />
+                            <div className={styles.label}>
+                                <label>Digite sua senha</label>
+                                <Link to="/"><label style={{
+                                    cursor
+                                        : "pointer"
+                                }}>Esqueceu sua senha?</label></Link>
+                            </div>
+                            <input type="password" placeholder="Digite sua senha" />
+                            <p>Não tem uma conta?</p>
+                            <Link to="/cadastre-se">Cadastre-se</Link>
+                            <button role='button'>Logar</button>
+                        </form>
+                    </div> 
+                    
+                    : 
+
+                    <div className={styles.containerLogin}>
+                        <h1>Login</h1>
+                        <div className={styles.btnCE}>
+                            <Button variant="outline-primary" onClick={handleCliente}>Cliente</Button>
+                            <Button variant="secondary" onClick={handleEmpresa}>Empresa</Button>
+                        </div>
+                        <form className={styles.form}>
+                            <label>Digite seu email</label>
+                            <input type="email" placeholder="Digite seu email" />
+                            <div className={styles.label}>
+                                <label>Digite sua senha</label>
+                                <Link to="/"><label style={{
+                                    cursor
+                                        : "pointer"
+                                }}>Esqueceu sua senha?</label></Link>
+                            </div>
+                            <input type="password" placeholder="Digite sua senha" />
+                            <p>Não tem uma conta?</p>
+                            <Link to="/cadastre-se">Cadastre-se</Link>
+                            <button role='button'>Logar</button>
+                        </form>
+                    </div> 
+                    
+                    
+                    }
+
 
                 <div className={styles.containerImage}>
-                    <LoginCarousels/>
+                    <LoginCarousels />
                 </div>
             </div>
         </>
