2e4f66a88b62f4f8a5cffde2153445c11c7f6ed0
Karolayne Rodrigues <karollayneerod@gmail.com>
2023-04-21 21:08:38 -0300

    footer

--- src/frontend/components/Footer.jsx
+++ src/frontend/components/Footer.jsx
@@ -1,24 +1,22 @@
-// Footer.jsx
 import React from 'react';
-import './Footer.css';
-
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 function Footer() {
   return (
     <footer>
-      <div className='elements'>
-        <ul> <h3>Contatos</h3>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-        <ul> <h3>Recicla.ai</h3>
-          <li><p>Somos uma empresa que conecta pessoas que <br /> reciclam à empresas de reciclagem</p></li>
-        </ul>
-        <ul> <h3>Recicla.ai</h3>
-          <li><p>Somos uma empresa que conecta pessoas que <br /> reciclam à empresas de reciclagem</p></li>
-        </ul>
+      <div className="icons">
+        <FaFacebook />
+        <FaTwitter />
+        <FaInstagram />
       </div>
+      <ul>
+        <h3>Recicla.ai</h3>
+        <li>
+          <p>
+            Somos uma empresa que conecta pessoas que reciclam à empresas de reciclagem
+          </p>
+        </li>
+      </ul>
     </footer>
   );
 }
