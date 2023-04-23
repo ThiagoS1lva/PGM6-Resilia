cdcf6c706cace201f30b4ea4b47aa8731576545d
Karolayne Rodrigues <karollayneerod@gmail.com>
2023-04-22 21:32:07 -0300

    footer

--- src/frontend/components/Footer.jsx
+++ src/frontend/components/Footer.jsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import './Footer.css';
 import { FaInstagram, FaLinkedin, FaEnvelope } from "react-icons/fa";
-import EmojiPicker from 'emoji-picker-react';
 
 function Footer() {
   return (
@@ -10,19 +9,25 @@ function Footer() {
       <div className='elements'>
         <ul> <h5>contato</h5>
           <li>
-            <FaInstagram/>Recicla.ai
+            <FaInstagram />Recicla.ai
           </li>
           <li>
-            <FaLinkedin/>Grupo-Reciclaai
+            <FaLinkedin />Grupo-Reciclaai
           </li>
           <li>
-            <FaEnvelope/>Recicla.ai@gmail.com
+            <FaEnvelope />Recicla.ai@gmail.com
+          </li>
+        </ul>   
+        <p className='copy' >&copy; 2023 - Recicla ai</p>
+        <ul>
+          <h3>Recicla.ai</h3>
+          <li>
+            <p>
+              Somos uma empresa que conecta pessoas que <br /> reciclam à empresas de reciclagem
+              </p>
           </li>
         </ul>
-        <ul> <h3>Recicla.ai</h3>
-          <li><p>Somos uma empresa que conecta pessoas que <br /> reciclam à empresas de reciclagem</p></li>
-        </ul>
-        <p>&copy; 2023 - Recicla ai</p> 
+     
       </div>
     </footer>
   );
