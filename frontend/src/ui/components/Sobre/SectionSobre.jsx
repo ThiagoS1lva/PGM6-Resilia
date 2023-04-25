bc5fb8fbbd90735c061e22b2e8fe29af15df4264
qu4resm4 <quaresmacont@gmail.com>
2023-04-25 03:14:00 -0300

    resolvendo conflitos

--- frontend/src/ui/components/Sobre/SectionSobre.jsx
+++ frontend/src/ui/components/Sobre/SectionSobre.jsx
@@ -1,16 +1,12 @@
 import Caixa from './CaixaTexto';
-<<<<<<< HEAD
 import styles from '../../styles/SectionSobre.module.css';
 import img from '../../../../public/teams.svg'
-=======
->>>>>>> a2b01a7c5e3a322cd8654ff3229e74e74ac92e29
 
 
 const SectionSobre = () => {
 
 
     return (
-<<<<<<< HEAD
         <div className={styles.sobre}>
             <div className={styles.sesquerda}>
                 
@@ -25,15 +21,6 @@ const SectionSobre = () => {
                 <Caixa className={styles.scaixa}/>
             </div>
         </div>
-=======
-        <>
-            
-                <Caixa/>
-                <div>
-                    
-                </div>
-        </>
->>>>>>> a2b01a7c5e3a322cd8654ff3229e74e74ac92e29
     );
 };
 
